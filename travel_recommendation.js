ae953f28e268b9b2f9b5e445ac5f416d380129ab
kuchela13 <christian.boodoo13@gmail.com>
2024-11-26 18:22:45 -0400

    style homepage

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,42 +1,67 @@
-const searchButton = document.getElementById("btnSearch");
-const clearButton = document.getElementById("btnClear")
-const resultsContainer = document.querySelector("results");
+const resultsContainer = document.getElementById("results");
 
 function getData() {
-fetch ("travel_recommendation_api.json")
-.then(response=>response.json())
-.then(data=> {
+    fetch("travel_recommendation_api.json")
+        .then(response => response.json())
+        .then(data => {
 
-    console.log(data);
-})
-.catch(error=>console.error("Error fetching data:", error));
+            console.log(data);
+        })
+        .catch(error => console.error("Error fetching data:", error));
 }
 
 //
-function getRec(keyword) {
-document.getElementById("search").value.toLowerCase();
+document.getElementById("btnSearch").addEventListener("click", getRec);
 
-getData().then (data=> {
-return data.filter(item=>item.category.includes("keyword"))
-let results=[];
+function getRec() {
+    document.getElementById("search").value.toLowerCase();
 
-    if ((keyword===beach)||(keyword===beaches)) {
-        results=data.beach
-    }
-    else if ((keyword===temple)||(keyword===temples)){
-    results=data.temple
-    } 
-    else if ((keyword===country)||(keyword===countries)) {
-    results=data.country
-    }
-    else {
-    resultsContainer="Please try again."
-    return  
-    }
 
-    displayResults(results);
-})
+    getData().then(data => {
+        data.filter(item => item.category.includes("keyword"));
+        let results = [];
+
+        if ((keyword === beach) || (keyword === beaches)) {
+            results = data.beach;
+        }
+        else if ((keyword === temple) || (keyword === temples)) {
+            results = data.temple;
+        }
+        else if ((keyword === country) || (keyword === countries)) {
+            results = data.country;
+        }
+        else {
+            resultsContainer.innerHTML = "Please try again.";
+            return;
+        }
+
+        displayResults(results);
+    });
 }
+
+function displayResults(results) {
+    resultsContainer.innerHTML = "";
+
+    recommendations.forEach(recommendation => {
+        const resultDiv = document.createElement('div');
+        resultDiv.innerHTML = `
+      <h3>${recommendation.name}</h3>
+      <img src="${recommendation.imageUrl}" alt="${recommendation.name}">
+      <p>${recommendation.description}</p>
+    `;
+        resultsContainer.appendChild(resultDiv);
+    });
+
+}
+function clearResults() {
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = '';
+}
+document.getElementById("btnClear").addEventListener('click', clearResults);
+
+
+
+
 
 
 
